bbf5d16aa4efcb734f78fb19e6563d26f51ea789
Elodie <elodie.dargot@gmail.com>
2023-10-13 17:13:09 +0200

    modification persistence/jakarta

--- src/main/java/com/wildcodeschool/wildandwizard/entity/School.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/School.java
@@ -1,11 +1,11 @@
 package com.wildcodeschool.wildandwizard.entity;
 
 import com.wildcodeschool.wildandwizard.repository.SchoolRepository;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 // TODO : update this entity
 @Entity
